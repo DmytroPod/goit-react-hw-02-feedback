2f6f834a306539752d0eaf6395e3aa64df793167
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-11-18 14:09:09 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,12 +1,12 @@
 import css from './Section.module.css';
 
-function Section({ title, children }) {
+const Section = ({ title, children }) => {
   return (
     <section className={css.section}>
       {title && <h2 className={css.title}>{title}</h2>}
       {children}
     </section>
   );
-}
+};
 
 export default Section;
